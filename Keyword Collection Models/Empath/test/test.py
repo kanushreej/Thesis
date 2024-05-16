d8a6af8223573c0cf9b291ca5fac3b1091ed42e3
kanushreej <90801546+kanushreej@users.noreply.github.com>
2024-05-16 21:01:16 +0200

    Delete Keyword Collection Models/Empath directory

--- Keyword Collection Models/Empath/test/test.py
+++
@@ -1,7 +0,0 @@
-from context import Empath
-
-empath = Empath()
-
-print(empath.analyze(["this","that"]))
-print(empath.analyze(["this","that"]))
-print(empath.analyze("the person typed on the keyboard"))

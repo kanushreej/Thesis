4db2c31d8a15452e465429bfc37741da68ff4a72
adamzulficar <a.zulficar@outlook.com>
2024-07-22 00:49:03 +0200

    analyses update

--- Analyses/Scripts/Preprocessing & Analyses/cluster_analyses.py
+++ Analyses/Scripts/Preprocessing & Analyses/cluster_analyses.py
@@ -7,42 +7,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-df = pd.read_csv('Analyses/User Data/usersUK_clustered_Kmeans.csv')
+df = pd.read_csv('Analyses/User Data/Clustered/usersUK_nr8.csv')
+region = 'UK'
 
-opinion_columns = [
-    'pro_brexit', 'anti_brexit',
-    'pro_climateAction', 'anti_climateAction',
-    'pro_NHS', 'anti_NHS',
-    'pro_israel', 'pro_palestine',
-    'pro_company_taxation', 'pro_worker_taxation',
-    'Brexit_neutral', 'ClimateChangeUK_neutral',
-    'HealthcareUK_neutral', 'IsraelPalestineUK_neutral',
-    'TaxationUK_neutral'
-]
-
-# opinion_columns = [
-#     'pro_immigration', 'anti_immigration',
-#     'pro_climateAction', 'anti_climateAction',
-#     'public_healthcare', 'private_healthcare',
-#     'pro_israel', 'pro_palestine',
-#     'pro_middle_low_tax', 'pro_wealthy_corpo_tax',
-#     'ImmigrationUS_neutral', 'ClimateChangeUS_neutral',
-#     'HealthcareUS_neutral', 'IsraelPalestineUS_neutral',
-#     'TaxationUS_neutral',
-# ]
-
-# Calculate mean values of opinion columns for each cluster
-cluster_means = df.groupby('topic')[opinion_columns].mean()
-print(cluster_means)
+if region == 'UK':
+    opinion_columns = [
+        'Brexit',
+        'ClimateChangeUK',
+        'HealthcareUK',
+        'IsraelPalestineUK',
+        'TaxationUK',
+    ]
+if region == 'US':
+        opinion_columns = [
+        'ImmigrationUS',
+        'ClimateChangeUS',
+        'HealthcareUS',
+        'IsraelPalestineUS',
+        'TaxationUS',
+    ]
 
 # Topic distribution
 topic_distribution = df['topic'].value_counts().sort_index()
 print(topic_distribution)
 
-# Optionally, display the topic distribution in a DataFrame for a better view
+# Display the topic distribution in a DataFrame for a better view
 topic_distribution_df = topic_distribution.reset_index()
 topic_distribution_df.columns = ['Topic', 'User Count']
 print(topic_distribution_df)
+
+# Calculate mean values of opinion columns for each cluster
+cluster_means = df.groupby('topic')[opinion_columns].mean()
+print(cluster_means)
 
 # Reduce dimensionality for visualization
 reducer = umap.UMAP(n_neighbors=5, min_dist=0.3, random_state=42)
@@ -56,10 +52,3 @@ plt.title('User Clusters based on Political Opinions')
 plt.xlabel('UMAP Dimension 1')
 plt.ylabel('UMAP Dimension 2')
 plt.show()
-
-# Plot the distribution of opinions within each cluster
-for column in opinion_columns:
-    plt.figure(figsize=(10, 6))
-    sns.boxplot(x='topic', y=column, data=df)
-    plt.title(f'Distribution of {column} across Clusters')
-    plt.show()
